6db5418a7ecd3b526ef48af68ce96798e34fad47
Nex Marshall <nexmarshall@gmail.com>
2021-06-27 19:59:33 +0530

    Started Basics

---
+++ index.js
@@ -0,0 +1,51 @@
+const Discord = require('discord.js');
+const bot = new Discord.Client();
+
+const { token } = require('./config.json');
+
+const { readdirSync } = require('fs');
+
+module.exports = bot;
+
+//!------------------------------------------------------Command Handler
+const ascii = require('ascii-table');
+const table = new ascii("Commands");
+table.setHeading("Command", "Load Status");
+for (const folder of commandFolders) {
+    const commandFiles = readdirSync(`./commands/${folder}`).filter(file => file.endsWith(".js"));
+    for (const file of commandFiles) {
+        const command = require(`./commands/${folder}/${file}`);
+        if (command.name) {
+            bot.commands.set(command.name, command);
+            table.addRow(file, '✅');
+        } else {
+            table.addRow(file, '❌');
+        }
+    }
+    console.log(table.toString());
+}
+bot.on("error", console.error);
+
+
+//!--------------------------------------------------------Event Handler
+
+readdirSync('./events/').forEach((file) => {
+    const events = readdirSync('./events/').filter((file) =>
+        file.endsWith(".js")
+    );
+
+    for (let file of events) {
+        let pull = require(`./events/${file}`);
+
+        if (pull.name) {
+            bot.events.set(pull.name, pull);
+        } else {
+            continue;
+        }
+    }
+})
+
+
+//-------------------------------------------------------------------------------------------------------------------------
+
+bot.login(token);
